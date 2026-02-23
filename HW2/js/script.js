f423fae36499503f0aeb0603cef11af5e4f7e1db
Paul <paultraissac04012006@gmail.com>
2026-02-23 04:31:32 +0100

    HW2

--- HW2/js/script.js
+++ HW2/js/script.js
@@ -1,16 +1,63 @@
 let wordArray = [
     ["P", "L", "A", "N", "T"],
-    ["S", "H", "O", "R", "E"],
     ["B", "R", "I", "C", "K"],
     ["M", "O", "U", "S", "E"],
-    ["A", "P", "P", "L", "E"]
+    ["A", "P", "P", "L", "E"],
+    ["S", "C", "A", "R", "F"]
 ]
 
-let wins = localStorage.getItem("win");
-let looses = localStorage.getItem("loose");
+let randomIndex;
 
-localStorage.setItem("win", wins);
-localStorage.setItem("loose", looses);
+let plant = document.createElement("img");
+plant.src = "ressources/plant.png";
+plant.alt = "Plant";
+plant.width = 50;
+plant.height = 50;
+plant.style.borderRadius = 20;
+
+let brick = document.createElement("img");
+brick.src = "ressources/brick.png";
+brick.alt = "Brick";
+brick.width = 50;
+brick.height = 50;
+brick.style.borderRadius = 20;
+
+let mouse = document.createElement("img");
+mouse.src = "ressources/mouse.png";
+mouse.alt = "Mouse";
+mouse.width = 50;
+mouse.height = 50;
+mouse.style.borderRadius = 20;
+
+let apple = document.createElement("img");
+apple.src = "ressources/apple.png";
+apple.alt = "Apple";
+apple.width = 50;
+apple.height = 50;
+apple.style.borderRadius = 20;
+
+let scarf = document.createElement("img");
+scarf.src = "ressources/scarf.png";
+scarf.alt = "Scarf";
+scarf.width = 50;
+scarf.height = 50;
+scarf.style.borderRadius = 20;
+
+let hint = document.querySelector("#hint");
+
+let wins = Number(localStorage.getItem("win"));
+
+if (!wins) { 
+    wins = 0;
+    localStorage.setItem("win", wins);
+}
+
+let looses = Number(localStorage.getItem("loose"));
+
+if (!looses) { 
+    looses = 0;
+    localStorage.setItem("loose", looses);
+}
 
 let userInput = document.querySelector("#userInput");
 let guessButton = document.querySelector("#guessButton");
@@ -25,7 +72,6 @@ let firstLetter = document.querySelector("#firstLetter");
 let secondLetter = document.querySelector("#secondLetter");
 let thirdLetter = document.querySelector("#thirdLetter");
 let fourthLetter = document.querySelector("#fourthLetter");
-let testedWords = document.querySelector("#testedWord");
 let fifthLetter = document.querySelector("#fifthLetter");
 
 let word = [
@@ -40,10 +86,31 @@ let correctWord;
 
 let numberOfTries;
 
-initiateGame();
+getHintButton.addEventListener("click", function() { 
+    hint.innerHTML = "";
+    getHintButton.style.display = "none";
+    if (randomIndex == 0) { 
+        hint.append(plant);
+    }
+    else if (randomIndex == 1) { 
+        hint.append(brick);
+    }
+    else if (randomIndex == 2) { 
+        hint.append(mouse);
+    }
+    else if (randomIndex == 3) { 
+        hint.append(apple);
+    }
+    else if (randomIndex == 4) { 
+        hint.append(scarf);
+    }
+    hint.style.display = "inline";
+})
 
 guessButton.addEventListener("click", checkGuess);
 retryButton.addEventListener("click", initiateGame);
+
+initiateGame();
 
 function initiateGame() { 
     for(i in word) { 
@@ -54,8 +121,10 @@ function initiateGame() {
     feedback.style.display = "inline";
     getHintButton.style.display = "inline";
     retryButton.style.display = "none";
-
-    let randomIndex = Math.floor(Math.random() * wordArray.length);
+    hint.style.display = "none";
+    getHintButton.style.display = "inline";
+    
+    randomIndex = Math.floor(Math.random() * wordArray.length);
 
     correctWord = wordArray[randomIndex];
 
@@ -68,7 +137,7 @@ function initiateGame() {
 function checkGuess() {
     if (userInput.value.length != 5) { 
         feedback.textContent = "the WORD is 5 letters long";
-        feedback.display = "inline";
+        feedback.style.display = "inline";
         return;
     }
 
@@ -97,6 +166,8 @@ function checkGuess() {
             }
             if (bool) { 
                 word[i].style.color = "orange";
+                arrayOfIndexOfCorrectLetters[index] = i;
+                index++;
             } else {
                 word[i].style.color = "red";
             }
@@ -116,27 +187,33 @@ function checkGuess() {
     }
 }
 
-function gameOverWin() { 
-    userInput.style.display = "none";
-    guessButton.style.display = "none";
-    feedback.style.display = "none";
-    getHintButton.style.display = "none";
-    retryButton.style.display = "inline";
-    localStorage.setItem("win", win++);
-    win.textContent = "Wins : " + localStorage.getItem("loose");
+function gameOverWin() {
+    gameOver();
+    localStorage.setItem("win", ++wins);
+    win.textContent = "Wins : " + localStorage.getItem("win");
 }
 
 function gameOverLoose() {
+    gameOver();
+    localStorage.setItem("loose", ++looses);
+    loose.textContent = "Looses : " + localStorage.getItem("loose");
+}
+
+function gameOver() { 
     userInput.style.display = "none";
     guessButton.style.display = "none";
     feedback.style.display = "none";
     getHintButton.style.display = "none";
     retryButton.style.display = "inline";
-    localStorage.setItem("loose", loose++);
-    loose.textContent = "Looses : " + localStorage.getItem("win");
+
+    document.querySelector("#testedWords").remove();
+    let testedWords = document.createElement("div");
+    testedWords.id = "testedWords";
+    document.querySelector("body").append(testedWords);
 }
 
 function appendWord() { 
+    let testedWords = document.querySelector("#testedWords");
     let wordText = document.querySelector("#userInput").value.toUpperCase();
     let p = document.createElement("p");
     p.textContent = wordText;
